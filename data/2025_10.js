e0a5a7e989f0e4e1c366e8641308ef758976ea9a
gc64kvdvpr-glitch <gc64kvdvpr@privaterelay.appleid.com>
2025-11-26 11:20:18 +0900

    Add files via upload

---
+++ data/2025_10.js
@@ -0,0 +1,218 @@
+window.MASTER_DATA.push(
+    {
+        passageId: 7,
+        category: "2025년 10월 모의고사",
+        slides: [
+            {
+                type: 'passage',
+                content: `Currently, [[urban regeneration projects|도시 재생 프로젝트|vocab]] in [[degraded areas|낙후된 지역|vocab]] [[have been promoted|추진되어 왔다 (현재완료 수동태)|grammar]] as {{[[improving|전치사(as)의 목적어 1 (동명사)|grammar]] the [[wellbeing|복지, 행복, 안녕|vocab]] of residents and [[solving|전치사(as)의 목적어 2 (동명사 병렬)|grammar]] environmental [[injustice|불평등|vocab]] problems}}.`
+            },
+            {
+                type: 'passage',
+                content: `However, such environmental improvements in [[ethnic|인종의, 민족의|vocab]] communities and/or low­income [[households|가정, 가구|vocab]] can create an [[urban green space paradox|도시 녹지 공간의 역설|vocab]]. The creation of new, high­quality green spaces can increase [[attractiveness|매력|vocab]], {{[[making|...하게 만들면서 (분사구문, 결과)|grammar]] these neighbourhood more [[desirable|살기 좋은, 바람직한|vocab]]}}.`
+            },
+            {
+                type: 'passage',
+                content: `By contrast, the cost of housing can rise, and residents may not be able [[to afford|~할 여유가 있다 (be able to afford)|grammar]] the rent. This results in the [[exclusion|배제|vocab]] or [[displacement|퇴거, 이동, 추방|vocab]] of the poor neighbourhood’s residents, {{[[who|주격 관계대명사 (residents 수식)|grammar]] [[were intended|의도되었다 (수동태)|grammar]] [[to benefit|~하도록 (intend 목적어 to V의 수동태)|grammar]] from the [[ecosystem|생태계|vocab]] services {{[[provided|제공되는 (services 수식 과거분사)|grammar]] by the new green space}}}}.`
+            },
+            {
+                type: 'passage',
+                content: `In turn, the residents may only be able [[to afford|감당하다|grammar]] [[to live|afford의 목적어 (to부정사)|grammar]] in a similar [[degraded|낙후된|vocab]] neighbourhood {{to the one {{[[they left|목적격 관계대명사 생략 (that they left)|grammar]]}}}}, with low access to green [[infrastructure|기반 시설|vocab]].`
+            },
+            {
+                type: 'quiz',
+                question: '7. 밑줄 친 an urban green space paradox가 다음 글에서 의미하는 바로 가장 적절한 것은? (모고 21번)',
+                options: [
+                    'an issue where sustainable technologies cost more in green areas',
+                    'a situation forcing out city residents green renewals aimed to help',
+                    'a result where green renovation harms the health of city dwellers',
+                    'a problem of city renovations making environmental pollution worse',
+                    'a circumstance causing more crimes in newly developed city ares'
+                ],
+                answer: 2
+            }
+        ]
+    },
+    {
+        passageId: 8,
+        category: "2025년 10월 모의고사",
+        slides: [
+            {
+                type: 'passage',
+                content: `Rome [[was said|말해졌다 (수동태)|grammar]] [[to have been|완료부정사 (to have p.p: 주절의 시제보다 더 과거의 일)|grammar]] a [[melting pot|용광로 (다양한 인종/문화의 융합)|vocab]] from the very start. The historian Livy claimed {{the city’s original population [[was comprised of|~로 구성되다|vocab]] immigrants [[flooding|immigrants를 수식 (현재분사: 능동)|grammar]] in from all directions, [[attracted|immigrants를 수식 (과거분사: 수동)|grammar]] by Romulus’s [[deliberate|의도적인|vocab]] policy of [[nondiscrimination|비차별|vocab]]}}.`
+            },
+            {
+                type: 'passage',
+                content: `[[It was|It ... that 강조 구문 (~한 것은 바로 ...였다)|grammar]] this [[initial|초기의|vocab]] openness, Livy asserts, [[that|It ... that 강조 구문|grammar]] [[laid|놓다 (lay-laid-laid)|vocab]] the [[foundations|기초, 토대|vocab]] for the later strength and success of the city. Romans described their city as [[multicultural|다문화적인|vocab]] in the generations after its [[foundation|건국, 창설|vocab]].`
+            },
+            {
+                type: 'passage',
+                content: `Tradition [[held|주장하다, 믿다|grammar]] {{[[that|접속사 (held의 목적절)|grammar]] only a minority of the city’s legendary kings were Roman­born, {{[[with|with + 명사 + 분사 (부대상황: ~한 채로)|grammar]] the others all [[arriving|능동 관계라 현재분사 사용|grammar]] as immigrants before [[being chosen|전치사(before) + 수동태 동명사 (선택받기 전에)|grammar]] for the [[throne|왕좌, 왕위|vocab]] for their [[virtues|미덕|vocab]] and [[merits|공로, 가치|vocab]]}}}}.`
+            },
+            {
+                type: 'passage',
+                content: `{{As the [[empire|제국|vocab]] expanded across three [[continents|대륙|vocab]]}}, Rome eagerly [[adopted|채택하다, 받아들이다|vocab]] new cultural influences and [[absorbed|흡수하다|vocab]] incoming groups―perhaps a little too eagerly for some, {{[[who|주격 관계대명사 (some을 수식, 계속적 용법)|grammar]], like the poet Juvenal, complained about the rapid rate of cultural change}}.`
+            },
+            {
+                type: 'quiz',
+                question: '8. 다음 글의 주제로 가장 적절한 것은? (모고 23번)',
+                options: [
+                    'cultural openness as a foundation for Rome’s growth',
+                    'cultural conflicts between native Romans and immigrants',
+                    'new foreign cultures as a replacement for Rome’s tradition',
+                    'difficulties in preserving Roman immigrants’ cultural identities',
+                    'political instability in Rome caused by rapid territorial expansion'
+                ],
+                answer: 1
+            }
+        ]
+    },
+    {
+        passageId: 9,
+        category: "2025년 10월 모의고사",
+        slides: [
+            {
+                type: 'passage',
+                content: `Human beings [[have evolved|진화해 왔다 (현재완료)|grammar]] to make the most of the resources {{[[available|resources를 뒤에서 수식 (형용사구)|grammar]] to them in ways {{[[that|주격 관계대명사 (ways 수식)|grammar]] are [[subtle|미묘한|vocab]] and complicated}}}}. When we change our diets, especially when we do so quickly, we are ①[[effectively|부사 (동사 conducting을 수식, 적절함)|grammar]] conducting huge experiments in [[nutrition|영양|vocab]].`
+            },
+            {
+                type: 'passage',
+                content: `We [[ought to|~해야 한다 (=should)|grammar]] have more [[humility|겸손|vocab]]. Nutritional science is still young and there is so much {{[[we do not know|목적격 관계대명사 생략 (that we...)|grammar]]}}. If we have eaten certain foods in certain ways for millennia, we should assume {{until it is [[proven|증명되다 (수동태)|grammar]] [[otherwise|그렇지 않다고|vocab]]}} ②[[what|어법상 틀림 (접속사 that이 적절: assume의 목적절)|grammar]] there is probably a good reason why.`
+            },
+            {
+                type: 'passage',
+                content: `[[Traditional|전통적인|vocab]] foods {{[[that|주격 관계대명사 (foods 수식)|grammar]] don’t fit neatly on the [[contemporary|현대의|vocab]] dietary food plate}} should generally [[be chosen|선택되어야 한다 (수동태)|grammar]] over highly processed ones {{[[that|주격 관계대명사 (ones 수식)|grammar]] ③[[do|대동사 (fit을 대신함, 적절함)|grammar]]}}.`
+            },
+            {
+                type: 'passage',
+                content: `We should assume {{[[that|접속사 (assume의 목적어 1)|grammar]] traditionally ④[[made|과거분사 (sausages 수식, 만들어진)|grammar]] fatty blood sausages are preferable to lean, factory­made salamis}}; {{[[that|접속사 (assume의 목적어 2, 병렬구조)|grammar]] spoonfuls of honey are [[superior to|~보다 뛰어난|vocab]] [[sprinkles|뿌리기, 소량|vocab]] of [[sweeteners|감미료|vocab]]}}.`
+            },
+            {
+                type: 'passage',
+                content: `[[Witnessing|동명사 주어 (단수 취급)|grammar]] {{how poorly traditional societies are [[faring|살아가다, 지내다|vocab]] {{as they [[undergo|겪다|vocab]] a nutrition [[transition|변화, 전환|vocab]]}}}} should [[make|사역동사 make (목적어+동사원형)|grammar]] those {{[[who|주격 관계대명사 (~하는 사람들)|grammar]] have already completed it}} ⑤[[question|동사원형 (make의 목적격 보어, 적절함)|grammar]] {{[[whether|~인지 아닌지 (명사절 접속사)|grammar]] their diets have moved too far}}.`
+            },
+            {
+                type: 'quiz',
+                question: '9. 다음 글의 밑줄 친 부분 중, 어법상 틀린 것은? (모고 29번)',
+                options: [
+                    'effectively',
+                    'what',
+                    'do',
+                    'made',
+                    'question'
+                ],
+                answer: 2
+            }
+        ]
+    },
+    {
+        passageId: 10,
+        category: "2025년 10월 모의고사",
+        slides: [
+            {
+                type: 'passage',
+                content: `While convenience and technology are crucial, they are not the only factors {{[[driving|factors를 수식 (현재분사: 능동)|grammar]] Gen Z’s financial decisions}}. This generation is incredibly [[values-driven|가치 중심적인|vocab]], and they want [[to bank|은행과 거래하다 (want의 목적어)|grammar]] with [[institutions|기관|vocab]] {{[[that|주격 관계대명사 (institutions 수식)|grammar]] match their personal beliefs and values}}.`
+            },
+            {
+                type: 'passage',
+                content: `[[Transparency|투명성|vocab]] is [[vital|중요한, 필수적인|vocab]]. [[Gen Z|Z세대|vocab]] is [[skeptical|회의적인|vocab]] of large corporations and institutions {{[[that|주격 관계대명사 (institutions 수식)|grammar]] ①[[lack|부족하다 (문맥상 적절)|vocab]] [[accountability|책임, 책무|vocab]]}}. They have [[grown up|성장해 왔다 (현재완료)|grammar]] in a world {{[[where|관계부사 (world 수식, 뒷문장 완전)|grammar]] information is freely available}}, and they expect complete transparency from the brands {{[[they support|목적격 관계대명사 생략 (that they support)|grammar]]}}.`
+            },
+            {
+                type: 'passage',
+                content: `Banks, for example, must ②[[clearly|명확하게 (동사 communicate 수식)|vocab]] communicate fees, [[terms|약관, 조건|vocab]], and [[conditions|조항|vocab]], as well as {{how they [[handle|의문사+주어+동사 (간접의문문/명사절)|grammar]] customers’ data}}. Moreover, [[ethical|윤리적인|vocab]] banking [[practices|관행|vocab]] are more ③[[important|중요한 (비교급 강조)|vocab]] than ever.`
+            },
+            {
+                type: 'passage',
+                content: `Gen Z cares about the environment, social justice, and the [[ethical|윤리적인|vocab]] [[implications|영향, 함의|vocab]] of their financial decisions. They are interested in {{[[sustainable|지속 가능한|vocab]] [[investing|전치사(in)의 목적어 1 (동명사)|grammar]]}}, {{[[supporting|목적어 2 (동명사)|grammar]] businesses {{[[that|주격 관계대명사 (businesses 수식)|grammar]] match their values}}}}, and {{[[ensuring|목적어 3 (동명사 병렬)|grammar]] {{[[that|접속사 (ensuring의 목적절)|grammar]] their money is not being used [[to fund|~자금을 대는 데 사용되다 (be used to V)|grammar]] ④[[beneficial|유익한 (문맥상 어휘 오류 → harmful)|vocab]] practices}}}}.`
+            },
+            {
+                type: 'passage',
+                content: `Banks {{[[that|주격 관계대명사 (Banks 수식)|grammar]] [[offer|제공하다 (동사 1)|vocab]] socially responsible investment opportunities and [[are committed to|~에 헌신하다 (동사 2)|grammar]] environmental sustainability}} will ⑤[[attract|끌다 (주어 Banks의 동사)|vocab]] Gen Z’s attention.`
+            },
+            {
+                type: 'quiz',
+                question: '10. 다음 글의 밑줄 친 부분 중, 문맥상 낱말의 쓰임이 적절하지 않은 것은? (모고 30번)',
+                options: [
+                    'lack',
+                    'clearly',
+                    'important',
+                    'beneficial',
+                    'attract'
+                ],
+                answer: 4
+            }
+        ]
+    },
+    {
+        passageId: 11,
+        category: "2025년 10월 모의고사",
+        slides: [
+            {
+                type: 'passage',
+                content: `Despite the cultural [[trope|비유적 표현|vocab]] {{[[depicting|trope를 수식 (현재분사: 묘사하는)|grammar]] emotions as the opposite of [[rational|합리적인|vocab]] thought}}, [[cognition|인지 (사고)|vocab]]―{{[[what|관계대명사 (~하는 것)|grammar]] we commonly [[refer to|말하다, 가리키다|vocab]] as thinking}}―is actually a key [[building block|구성 요소|vocab]] of emotion.`
+            },
+            {
+                type: 'passage',
+                content: `{{[[How we think|명사절 주어 (우리가 생각하는 방식)|grammar]] about our [[circumstances|상황|vocab]]}} [[shapes|형성하다 (주어가 3인칭 단수 취급)|grammar]] the emotions {{[[we experience|목적격 관계대명사 생략 (that we...)|grammar]]}}; then those emotions echo back [[to influence|결과적 용법 (~해서 영향을 주다)|grammar]] {{how we think}}.`
+            },
+            {
+                type: 'passage',
+                content: `For instance, {{if you walk into a test [[thinking|...라고 생각하며 (동시동작 분사구문)|grammar]] {{ (that) you are bad at taking tests }}}}$, your anxiety will be increased. Then you don’t feel good about your performance on the test, and that becomes evidence for [[continuing|전치사(for)의 목적어 (동명사)|grammar]] [[to think|continue의 목적어 (to부정사)|grammar]] {{that you’re bad at test taking}}.`
+            },
+            {
+                type: 'passage',
+                content: `In this way _________. This [[bi-directionality|양방향성|vocab]] of cognition and emotion [[allows|allow A to B (A가 B하게 하다)|grammar]] us [[to adjust|목적격 보어 (to부정사)|grammar]] difficult emotions by [[changing|~함으로써 (by + 동명사)|grammar]] {{the way we think}}.`
+            },
+            {
+                type: 'passage',
+                content: `{{By [[thinking|~함으로써 (by + 동명사)|grammar]] differently―I get nervous sometimes, but I’m still a good test taker, or that nervous feeling is just excitement and anticipation, it means I’m ready}}―you can work those pathways to your [[advantage|이점, 유리함|vocab]].`
+            },
+            {
+                type: 'quiz',
+                question: '11. 다음 빈칸에 들어갈 말로 가장 적절한 것을 고르시오. (모고 33번)',
+                options: [
+                    'your emotion will not always be conveyed as intended',
+                    'there’s simply no pulling emotion and cognition apart',
+                    'the ability to regulate emotions can’t be learned',
+                    'no two people feel an emotion the same way',
+                    'emotion often travels far away from reason'
+                ],
+                answer: 2
+            }
+        ]
+    },
+    {
+        passageId: 12,
+        category: "2025년 10월 모의고사",
+        slides: [
+            {
+                type: 'passage',
+                content: `Multisensory experiences are a central part of our everyday lives, yet we often [[take them for granted|~을 당연하게 여기다|vocab]], especially {{when our senses [[function|기능하다, 작동하다|vocab]] normally or [[are corrected|교정되다 (수동태)|grammar]] to normal with aids like glasses}}.`
+            },
+            {
+                type: 'passage',
+                content: `(A) And it does not stop there. Even the sounds {{[[that|주격 관계대명사 (sounds 수식)|grammar]] come both from the atmospheres {{[[in which|전치사 + 관계대명사 (atmospheres 수식, =where)|grammar]] we eat}} and our interactions with the food (such as chewing) and the tools {{[[we use|목적격 관계대명사 생략 (that we use)|grammar]] to eat}}}} [[influence|본동사 (주어: Even the sounds)|grammar]] our eating experience.`
+            },
+            {
+                type: 'passage',
+                content: `(B) However, closer inspection to any, even the most ordinary experiences, reveals the remarkable [[multisensory|다감각적인|vocab]] world {{[[in which|전치사 + 관계대명사 (world 수식, =where)|grammar]] we live}}. Consider the experience of eating a regular meal.`
+            },
+            {
+                type: 'passage',
+                content: `(C) At first, it may seem like an ordinary experience, but it is actually a [[fusion|융합|vocab]] of the senses. We first eat with our eyes, but we are also [[exposed to|~에 노출되다 (수동태)|grammar]] [[countless|수많은|vocab]] sensory signals {{[[that|주격 관계대명사 (signals 수식)|grammar]] influence our eating experience such as food [[textures|질감|vocab]], tastes, and smells}}.`
+            },
+            {
+                type: 'quiz',
+                question: '12. 주어진 글 다음에 이어질 글의 순서로 가장 적절한 것을 고르시오. (모고 36번)',
+                options: [
+                    '(A)-(C)-(B)',
+                    '(B)-(A)-(C)',
+                    '(B)-(C)-(A)',
+                    '(C)-(A)-(B)',
+                    '(C)-(B)-(A)'
+                ],
+                answer: 3
+            }
+        ]
+    }
+);
