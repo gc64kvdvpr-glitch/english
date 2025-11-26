cf4cb64fe7a4c2a28045dbc5dacc7d35b3fb3de4
gc64kvdvpr-glitch <gc64kvdvpr@privaterelay.appleid.com>
2025-11-26 11:12:30 +0900

    Add files via upload

---
+++ 모의고사 분석 자료/data/2024_10.js
@@ -0,0 +1,210 @@
+window.MASTER_DATA.push(
+    {
+        passageId: 1,
+        category: "2024년 10월 모의고사",
+        slides: [
+            {
+                type: 'passage',
+                content: `When we see something, we naturally and [[automatically|자동적으로|vocab]] break it up into shapes, colors, and [[concepts|개념|vocab]] {{[[that|목적격 관계대명사절 (concepts 수식)|grammar]] we [[have learned|현재완료|grammar-alt]] through education}}.`
+            },
+            {
+                type: 'passage',
+                content: `We [[recode|재부호화하다|vocab]] [[what we see|선행사 포함 관계대명사 (~하는 것)|grammar-alt]] through the lens of everything {{[[we know|목적격 관계대명사 생략 (that we know)|grammar]]}}. We [[reconstruct|재구성하다|vocab]] memories rather than {{retrieving the video from memory}}. This is a useful [[trait|특성|vocab]]. It’s a more [[efficient|효율적인|vocab]] way {{[[to store|to부정사(형용사적)|grammar]] information}} ― a bit like an [[optimal|최적의|vocab]] image [[compression|압축|vocab]] [[algorithm|알고리즘|vocab]] such as JPG, rather than {{storing a raw bitmap image file}}.`
+            },
+            {
+                type: 'passage',
+                content: `People {{[[who|주격 관계대명사 절 (People 수식)|grammar]] lack this ability and remember everything in perfect detail}} [[struggle|~하는 데 어려움을 겪다 (struggle to V)|vocab]] to generalize, learn, and make [[connections|연결|vocab]] between {{[[what they have learned|선행사 포함 관계대명사|grammar-alt]]}}. But {{[[representing the world as abstract ideas and features|동명사 주어 (단수 취급)|grammar]]}} [[comes at a cost|대가를 치르다|vocab]] of seeing the world [[as it is|있는 그대로|grammar-alt]].`
+            },
+            {
+                type: 'passage',
+                content: `Instead, we see the world through our [[assumptions|가정|vocab]], [[motivations|동기|vocab]], and past experiences. The discovery {{[[that|동격의 that 접속사 (discovery의 내용)|grammar]] our memories [[are reconstructed|수동태|grammar]] through abstract representations rather than played back like a movie}} completely [[undermined|손상시키다|vocab]] the legal [[primacy|우위성|vocab]] of [[eyewitness|목격자|vocab]] [[testimony|증언|vocab]]. [[Seeing is not believing|Seeing: 동명사 주어|grammar]].`
+            },
+            {
+                type: 'quiz',
+                question: '1. 밑줄 친 Seeing is not believing.이 다음 글에서 의미하는 바로 가장 적절한 것은? (모고 21번)',
+                options: [
+                    '[[Abstract|추상적인|vocab]] ideas are hard to explain without relevant images.',
+                    'It takes longer to retrieve unconsciously encoded information.',
+                    'Beliefs formed from repeated experiences do not easily change.',
+                    'Our memories fall short of an objective representation of the world.',
+                    'Comprehension of facts precedes the formation of abstract concepts.'
+                ],
+                answer: 4
+            }
+        ]
+    },
+    {
+        passageId: 2,
+        category: "2024년 10월 모의고사",
+        slides: [
+            {
+                type: 'passage',
+                content: `Many forms of research lead naturally to [[quantitative|양적인|vocab]] data. A study of happiness might [[measure|측정하다|vocab]] the number of [[times|관계부사 생략|grammar]] {{someone smiles during an [[interaction|상호 작용|vocab]]}}, and a study of memory might measure the number of [[items|선행사 (뒤에 목적격 관계대명사 that/which가 생략됨)|grammar]] {{an individual can recall after one, five, and ten minutes}}.`
+            },
+            {
+                type: 'passage',
+                content: `{{[[Asking|동명사 주어|grammar]] people [[how many times in a year they are sad|간접의문문(의문사+주어+동사)|grammar-alt]]}} will also yield [[quantitative|양적인|vocab]] data, but it might not be reliable. Respondents’ [[recollections|회상|vocab]] may be [[inaccurate|부정확한|vocab]], and their [[definitions|정의|vocab]] of ‘sad’ could [[vary|다르다|vocab]] widely.`
+            },
+            {
+                type: 'passage',
+                content: `But {{[[asking|동명사 주어 (단수 취급되어 동사 prompts 사용)|grammar]] “How many times in the past year were you sad [[enough to call|형용사 + enough to V: ~할 만큼 충분히 ...한|grammar]] in sick to work?”}} [[prompts|유발하다|vocab]] a [[concrete|구체적인|vocab]] answer.`
+            },
+            {
+                type: 'passage',
+                content: `Similarly, instead of {{[[asking|전치사(of)의 목적어로 쓰인 동명사|grammar]] people [[to rate|ask 목적어 to부정사|grammar-alt]] how bad a [[procrastinator|미루는 사람|vocab]] they are}}, ask, “How many of your [[utility|공과금|vocab]] bills are you currently late in [[paying|전치사(in)의 목적어로 쓰인 동명사|grammar]], even though you can [[afford to|~할 여유가 있다|grammar]] pay them?” Questions {{[[that|주격 관계대명사 (선행사 Questions 수식)|grammar]] seek concrete [[responses|응답|vocab]]}} [[help|준사역동사|grammar]] [[make|목적격 보어1|verb]] [[abstract|추상적인|vocab]] concepts clearer and [[ensure|목적격 보어2|verb]] [[consistency|일관성|vocab]] from one study to the next.`
+            },
+            {
+                type: 'quiz',
+                question: '2. 다음 글의 주제로 가장 적절한 것은?',
+                options: [
+                    'risks of overgeneralizing results from the collected data',
+                    'usefulness of answering abstract questions with numbers',
+                    'effect of sample size on enhancing the reliability of research',
+                    'limitations of measuring and quantifying various human emotions',
+                    'importance of specific questions to attain reliable quantitative data'
+                ],
+                answer: 5
+            }
+        ]
+    },
+    {
+        passageId: 3,
+        category: "2024년 10월 모의고사",
+        slides: [
+            {
+                type: 'passage',
+                content: `Digital technologies are [[essentially|본질적으로|vocab]] related to [[metaphors|은유, 비유|vocab]], but digital metaphors are different from linguistic ①[[ones|metaphors를 받는 대명사|grammar]] in important ways. Linguistic metaphors are [[passive|수동적인|vocab]], [[in the sense that|~라는 점에서|grammar-alt]] {{the audience needs [[to choose|need의 목적어 (to부정사 명사적 용법)|grammar]] to actively enter the world [[proposed|제안된 (world를 수식하는 과거분사)|grammar]] by [[metaphor|은유, 비유|vocab]]}}.`
+            },
+            {
+                type: 'passage',
+                content: `In the Shakespearean metaphor “time is a beggar,” the audience is [[unlikely|~할 것 같지 않은|vocab]] to understand the metaphor without [[cognitive|인지의|vocab]] effort and without further ②[[engaging|전치사(without)의 목적어 (동명사)|grammar]] Shakespeare’s [[prose|산문|vocab]]. Technological metaphors, [[on the other hand|반면에|vocab]], are active (and often [[imposing|부담을 주는, 인상적인|vocab]]) [[in the sense that|~라는 점에서|grammar-alt]] {{they [[are realized|실현되다 (수동태)|grammar]] in [[digital artifacts|디지털 인공물|vocab]] {{[[that|주격 관계대명사 (artifacts 수식)|grammar]] are actively doing things, forcefully ③[[changing|...하며 (분사구문, and forcefully change...)|grammar]] a user’s [[meaning horizon|의미의 지평|vocab]]}}}}.`
+            },
+            {
+                type: 'passage',
+                content: `Technological creators cannot generally [[afford|~할 여유가 있다 (주로 can't afford to)|vocab]] [[to require|afford의 목적어 (to부정사)|grammar]] their potential audience [[to wonder|require A to B (A에게 B할 것을 요구하다)|grammar]] how the metaphor works; normally the selling point is ④[[what|명사절을 이끄는 접속사 that이 와야 함|grammar]] {{the usefulness of the technology is [[obvious|분명한|vocab]] [[at first glance|처음에는|vocab]]}}. Shakespeare, [[on the other hand|반면에|vocab]], is [[beloved|사랑받는|vocab]] in part {{because the meaning of his works is not [[immediately|즉시|vocab]] [[obvious|분명한|vocab]] and ⑤[[requires|...is... and requires... (동사 병렬 구조)|grammar]] some thought on the part of the audience}}.`
+            },
+            {
+                type: 'quiz',
+                question: '3. 다음 글의 밑줄 친 부분 중, 어법상 틀린 것은? (모고 29번)',
+                options: [
+                    'ones',
+                    'engaging',
+                    'changing',
+                    'what',
+                    'requires'
+                ],
+                answer: 4
+            }
+        ]
+    },
+    {
+        passageId: 4,
+        category: "2024년 10월 모의고사",
+        slides: [
+            {
+                type: 'passage',
+                content: `Herbert Simon won his [[Nobel Prize|노벨상|vocab]] for [[recognizing|전치사(for)의 목적어 (동명사)|grammar]] our [[limitations|한계, 제약|vocab]] in information, time, and [[cognitive|인지의|vocab]] [[capacity|용량, 능력|vocab]]. {{As we [[lack|부족하다|vocab]] the [[resources|자원|vocab]] [[to compute|to부정사 형용사적 용법|grammar]] answers [[independently|독립적으로|vocab]]}}, we ①[[distribute|분배하다 (문맥상 어휘 ①)|verb]] the [[computation|계산|vocab]] across the population and [[solve||verb]] the answer slowly, generation by generation.`
+            },
+            {
+                type: 'passage',
+                content: `Then {{all we have to do}} is socially [[learn|'all... is (to) learn' (to부정사의 to가 생략된 보어)|grammar]] the right answers. You don’t need [[to understand|need의 목적어 (to부정사 명사적 용법)|grammar]] {{how your computer or toilet works}}; you just need to be able to [[use||verb]] [[interface|인터페이스, 접점|vocab]] and [[flush|(변기) 물을 내리다|verb]].`
+            },
+            {
+                type: 'passage',
+                content: `All {{[[that|주격 관계대명사 (All 수식)|grammar]] needs [[to be|전달될 필요가 있다 (to부정사 수동태)|grammar]] ②[[transmitted|전송하다 (문맥상 어휘 ②)|vocab]]}} is {{[[which|의문형용사 (어느 버튼을)|grammar]] button [[to push|의문사 + to V (~해야 할지)|grammar]]}}―[[essentially|본질적으로|vocab]] {{how [[to interact|의문사 + to V (~하는 방법)|grammar]] with technologies}} rather than {{how they work}}.`
+            },
+            {
+                type: 'passage',
+                content: `And so instead of [[holding|전치사(of)의 목적어 (동명사)|grammar]] ③[[less|문맥상 어휘 ③ (→ more)|grammar]] information {{than we have mental [[capacity|능력|vocab]] for and [[indeed|사실, 정말로|vocab]] need to know}}, we could [[dedicate|바치다, 헌신하다|vocab]] our large brains to a small piece of a giant [[calculation|계산|vocab]].`
+            },
+            {
+                type: 'passage',
+                content: `We understand things well [[enough to|형용사/부사 + enough to V (~할 만큼 충분히)|grammar]] ④benefit from them, but [[all the while|그러면서|grammar]] we are making small [[calculations|계산|vocab]] {{[[that|주격 관계대명사 (calculations 수식)|grammar]] [[contribute to|~에 기여하다|vocab]] a larger whole}}. We are just doing our part in a larger computation for our societies’ ⑤[[collective|집단적인 (문맥상 어휘 ⑤)|vocab]] brains.`
+            },
+            {
+                type: 'quiz',
+                question: '4. 다음 글의 밑줄 친 부분 중, 문맥상 낱말의 쓰임이 적절하지 않은 것은? (모고 30번)',
+                options: [
+                    'distribute',
+                    'transmitted',
+                    'less',
+                    'benefit',
+                    'collective'
+                ],
+                answer: 3
+            }
+        ]
+    },
+    {
+        passageId: 5,
+        category: "2024년 10월 모의고사",
+        slides: [
+            {
+                type: 'passage',
+                content: `The best [[defence|방어|vocab]] {{most species of octopus have}} is [[to stay|보어 (to부정사 명사적 용법, ~하는 것)|grammar]] [[hidden|숨겨진 (상태)|vocab]] [[as much as possible|가능한 한 많이|vocab]] and do their own hunting at night. So [[to find|주어 (to부정사 명사적 용법, ~하는 것)|grammar]] [[one|octopus|grammar-alt]] [[in full view|훤히 보이는 곳에|vocab]] in the [[shallows|얕은 곳|vocab]] in daylight was a surprise for two Australian underwater photographers.`
+            },
+            {
+                type: 'passage',
+                content: `Actually, {{[[what|주어 (~하는 것, the thing which)|grammar]] they saw at first}} was a [[flounder|넙치|vocab]]. [[It|It... that 강조 구문 (It was ~ that ...)|grammar]] was {{only when they looked again}} [[that|It... that 강조 구문 (부사절 강조)|grammar]] they saw a medium­sized octopus, {{[[with|with + 명사 + 분사 (부대-상황)|grammar]] all eight of its arms [[folded|팔이 '접힌' (수동, 과거분사)|grammar]] and its two eyes [[staring|눈이 '응시하는' (능동, 현재분사)|grammar]] upwards to __________ }}.`
+            },
+            {
+                type: 'passage',
+                content: `An octopus has a big brain, excellent eyesight and the [[ability|능력|vocab]] [[to change|to부정사 형용사적 용법|grammar]] colour and pattern, and this [[one|octopus|grammar-alt]] [[was using|...을 사용하고 있었다 (진행형)|grammar]] these [[assets|자산, 장점|vocab]] [[to turn|~하기 위해 (to부정사 부사적 용법 - 목적)|grammar]] itself into a completely different [[creature|생물체|vocab]].`
+            },
+            {
+                type: 'passage',
+                content: `Many more of this species [[have been found|발견되어 왔다 (현재완료 수동태)|grammar]] since then, and there are now photographs of octopuses {{[[that|주격 관계대명사|grammar]] could [[be said|...라고 말해질 수 있다 (수동태)|grammar-alt]] [[to be transforming|...으로 변신하고 있다고 (to be + V-ing)|grammar]] into [[sea snakes|바다뱀|vocab]]}}.`
+            },
+            {
+                type: 'passage',
+                content: `And while they [[mimic|흉내내다|vocab]], they hunt―[[producing|분사구문|grammar]] the [[spectacle|장관, 광경|vocab]] of, [[say|말하자면, 예를 들어|grammar-alt]], a [[flounder|동명사의 의미상 주어|vocab]] suddenly [[developing|동명사1|verb]] an [[octopodian arm|문어 다리|vocab]], [[sticking|동명사2|verb]] it [[down a hole|~을 구멍에 넣다|vocab]] and [[grabbing|동명사3|verb]] {{[[whatever|~하는 것은 무엇이든 (복합관계대명사)|grammar]]’s hiding there}}.`
+            },
+            {
+                type: 'quiz',
+                question: '5. 다음 빈칸에 들어갈 말로 가장 적절한 것을 고르시오. (모고 31번)',
+                options: [
+                    'get a broad view',
+                    'create the illusion',
+                    'capture the moment',
+                    'find its hiding spot',
+                    'mark its territory'
+                ],
+                answer: 2
+            }
+        ]
+    },
+    {
+        passageId: 6,
+        category: "2024년 10월 모의고사",
+        slides: [
+            {
+                type: 'passage',
+                content: `Conventional [[medicine|의학|vocab]] [[has long believed|믿어왔다 (현재완료)|grammar]] {{[[that|접속사 (believed의 목적어 명사절)|grammar]] [[depression|우울증|vocab]] [[is caused|유발된다 (수동태)|grammar]] by an [[imbalance|불균형|vocab]] of [[neurotransmitters|신경 전달 물질|vocab]] in the brain}}.`
+            },
+            {
+                type: 'passage',
+                content: `(A) However, there is a major problem with this [[explanation|설명|vocab]]. This is {{because the [[imbalance|불균형|vocab]] of [[substances|물질|vocab]] in the brain is a [[consequence|결과|vocab]] of depression, not [[its|brain's|grammar]] [[cause|원인|vocab]]}}. In other words, depression [[causes|원인이 되다|vocab]] a [[decrease|감소|vocab]] in brain [[substances|물질|vocab]] such as serotonin and noradrenaline, not a decrease in brain substances causes depression.`
+            },
+            {
+                type: 'passage',
+                content: `(B) If [[it|brain|grammar]] is not [[consciousness|의식|vocab]] [[itself|그자체|grammar]], then the [[root cause|근본 원인|vocab]] of depression is also a [[distortion|왜곡|vocab]] of our state of [[consciousness|의식|vocab]]: a consciousness {{[[that|주격 관계대명사|grammar]] has lost its sense of self and the meaning of life}}. Such a disease of consciousness may [[manifest|나타내다|vocab]] itself in the form of depression.`
+            },
+            {
+                type: 'passage',
+                content: `(C) In this [[revised|수정된|vocab]] [[cause-and-effect|인과관계|vocab]], the key is [[to reframe|보어 (to부정사 명사적 용법, ~하는 것)|grammar]] depression as a problem of [[consciousness|의식|vocab]]. Our consciousness is a more [[fundamental|근본적인|vocab]] [[entity|실체|vocab]] {{[[that|주격 관계대명사|grammar]] [[goes beyond|~을 넘어서다|vocab]] the [[functioning|기능|vocab]] of the brain}}. The brain is [[no more than|~에 지나지 않는|vocab]] an [[organ|장기, 기관|vocab]] of consciousness.`
+            },
+            {
+                type: 'quiz',
+                question: '6. 주어진 글 다음에 이어질 글의 순서로 가장 적절한 것을 고르시오. (모고 37번)',
+                options: [
+                    '(A)―(C)―(B)',
+                    '(B)―(A)―(C)',
+                    '(B)―(C)―(A)',
+                    '(C)―(A)―(B)',
+                    '(C)―(B)―(A)'
+                ],
+                answer: 1
+            }
+        ]
+    }
+);
